54d2d115449da0662302853cc7588bc31b452939
amiblog <reza.am.0425@gmail.com>
2022-01-27 13:43:07 +0000

    非同期通信途中

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,9 +5,8 @@ class BookCommentsController < ApplicationController
     comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
   end
-  
+
   def destroy
     BookComment.find(params[:id]).destroy
     redirect_to request.referer
